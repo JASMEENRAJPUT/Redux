2b0318b972d7938df2b8480dbc233e4e2cfd4bfd
JASMEENRAJPUT <jasmeenrajput844@gmail.com>
2026-06-20 18:46:05 +0530

    Add Reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useSelector , useDispatch} from "react-redux";
-import {increment ,  decrement} from './features/counterSlice';
+import {useSelector , useDispatch} from 'react-redux'
+import {increment, decrement, reset} from './features/counterSlice';
 
-function App(){
-  const count = useSelector((state)=>state.counter.count);
-
+export default function App(){
+  const count = useSelector((state)=> state.counter.count);
   const dispatch = useDispatch();
 
   return(
     <>
     <h1>{count}</h1>
+
     <button onClick={()=>dispatch(increment())}>
-      +
+      Increase
     </button>
     <button onClick={()=>dispatch(decrement())}>
-      -
+      Decrease
+    </button>
+    <button onClick={()=>dispatch(reset())}>
+      Reset
     </button>
     </>
-  );
+  )
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux'
 import {store} from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <provider store ={store}>
+  <Provider store ={store}>
     <App />
-  </provider>,
+  </Provider>
 )
